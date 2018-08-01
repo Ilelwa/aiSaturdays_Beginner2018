9c38327bd6dd88c539189f49fba9b62436399b55
Ilelwa <36058527+Ilelwa@users.noreply.github.com>
2018-08-01 21:56:10 +0300

    Revert "Update task1.py"

--- task1.py
+++ task1.py
@@ -1,15 +1,9 @@
 import random 
 
 ourList = list()
-belowFive = list()
 count = 0 
 while (count < 11):
     ourList.append(random.randint(1,10))
     count += 1
-   
-if (val<5):
     
-   belowFive.append(val)
-   
-print(ourList)
-print(belowFive)
+ourList
